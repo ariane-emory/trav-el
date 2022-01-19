925bcf4dda51ba2f4961226dbc3a8dab68ed4753
ariane-emory <ariane.emory@protonmail.com>
2022-01-18 21:15:29 -0500

    ...

--- trav.el
+++ trav.el
@@ -430,11 +430,21 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun insert-new-uwp-and-description ()
+(defun insert-new-uwp-with-description ()
   (interactive)
   (let ((u (create-uwp)))
     (insert
-      (as-code u) ":\n"
+      (as-code u) ":\n\n"
+      (pretty-format u)
+      "\n")))
+
+(defun describe-uwp-at-point ()
+  (interactive)
+  (setq u (parse-uwp (thing-at-point 'word 'no-properties)))
+  (save-excursion
+    (insert
+      "\n"
       (pretty-format u))))
 
-(global-set-key (kbd "C-c C-u C-i") 'insert-new-uwp)
+(global-set-key (kbd "C-c C-u C-d") 'describe-uwp-at-point)
+(global-set-key (kbd "C-c C-u C-i") 'insert-new-uwp-with-description)
