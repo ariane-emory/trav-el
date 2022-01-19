d972d9a3f973af81a9e68a35b0eaaa3c26082145
ariane-emory <ariane.emory@protonmail.com>
2022-01-19 06:07:32 -0500

    ...

--- trav.el
+++ trav.el
@@ -1,9 +1,42 @@
-;;; trav.el --- Useful tools for the a classic science fiction RPG.
-;; -*- lisp-indent-offset: 2; -*-
-;; -*- fill-column: 60;-*-
-;; -*- lexical-binding: t-*-
+;;; trav.el --- Useful tools for the a classic science fiction RPG. -*- lexical-binding: t-*-
 
-;; Copyright (c) 2022 Ariane Emory <ariange.emory@protonmail.com/
+;; Copyright (c) 2022 Ariane Emory <ariane.emory@protonmail.com>
+
+;; Author: Ariane Emory <ariane.emory@protonmail.com;; URL: https://github.com/ariane-emory/trav-el
+;; Package-Requires: (dash)
+;; Package-Version: 20220119
+;; Keywords: games
+;; Version: 0.1.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A mode with useful tools for Traveller-like RPGs such as Cepheus Engine.
+;;
+;; Currently all it does is generate UWPs and produce descriptions for them, but it will probably do more things in the future, like generating entire SEC files and maybe trade results.
+;;
+;; This is a minor mode. Turn it on with:
+;; ┌────
+;; │ (trav-mode 1)
+;; └────
+;;
+;; To insert a new, randomly generated UWP at point, hit 'C-c C-u C-i'.
+;;
+;; To produce a description of a UWP, place your cursor on it and hit 'C-c C-u C-d'.
+
+;;; Code:
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -62,3 +95,9 @@
 (define-minor-mode trav-mode
   "A mode with useful tools for Traveller-like RPGs such as Cepheus Engine."
   :lighter " ✹")
+;;; trav.el ends here
+
+;; Local Variables:
+;; lisp-indent-offset: 2
+;; fill-column: 60
+;; End:
