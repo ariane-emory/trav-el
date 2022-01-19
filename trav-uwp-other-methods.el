b4fb585824810cceb66cf8a8039260de0126da28
ariane-emory <ariane.emory@protonmail.com>
2022-01-18 23:36:08 -0500

    ...

---
+++ trav-uwp-other-methods.el
@@ -0,0 +1,23 @@
+;; -*- lisp-indent-offset: 2; -*-
+;; -*- fill-column: 60;-*-
+;; -*- lexical-binding: t-*-
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Other methods
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(cl-defmethod map-slots ((obj uwp) fun)
+  "Call fun on every slot of UWP in order."
+  (mapcar fun uwp--slot-names))
+
+(cl-defmethod as-code ((obj uwp))
+  "Print a UWP object."
+  (string-join
+    (cons
+      (cdr (assoc (slot-value obj 'starport)
+             uwp--starport-descriptions-alist))
+      (cdr (map-slots obj
+             (lambda (slot-name)
+               (t-format (slot-value obj slot-name))))))))
+  
+(provide 'trav-uwp-other-methods)
